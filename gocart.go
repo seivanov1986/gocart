c275809453687d867c6ff29d8c58e39ea4c7192a
Stanislav Ivanov <seivanov@avito.ru>
2023-12-14 16:22:01 +0300

    add http create

--- gocart.go
+++ gocart.go
@@ -1,11 +1,9 @@
 package gocart
 
 import (
-	"net/http"
-
 	"github.com/seivanov1986/gocart/internal/http/user"
 )
 
-func UserCreateHttpHandler() func(http.ResponseWriter, *http.Request) {
-	return user.New().Create
+func UserHttpHandler() user.User {
+	return user.New()
 }
