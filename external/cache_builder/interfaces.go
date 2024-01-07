5c562b7ace7ff29c466a4bbe47dc21602222a6b5
Stanislav Ivanov <seivanov@avito.ru>
2024-01-07 20:09:36 +0300

    fix

--- external/cache_builder/interfaces.go
+++ external/cache_builder/interfaces.go
@@ -2,8 +2,11 @@ package cache_builder
 
 import (
 	"context"
+
+	"github.com/seivanov1986/gocart/client"
 )
 
 type Service interface {
 	Make(ctx context.Context) error
+	MakeObject(ctx context.Context, row client.SefUrlItem) ([]byte, error)
 }
